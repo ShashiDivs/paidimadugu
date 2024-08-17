a4423a9d09b7e8e5d17348ca32e8dca399cf161b
shashi <gaajarlasheshugoud@gmail.com>
2024-08-17 21:24:33 +0530

    production ready for sample

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import openpyxl
 
-# hide_github_icon = """
-# <style>
-# .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK, 
-# #MainMenu, footer, header {
-#   display: none !important;  // Ensures these elements are completely removed from view
-# }
-# </style>
-# """
-# st.markdown(hide_github_icon, unsafe_allow_html=True)
+hide_github_icon = """
+<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK, 
+#MainMenu, footer, header {
+  display: none !important;  // Ensures these elements are completely removed from view
+}
+</style>
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 
 import matplotlib.pyplot as plt
